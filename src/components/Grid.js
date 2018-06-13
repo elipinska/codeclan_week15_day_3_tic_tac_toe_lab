0a2c62c19cccd01ae8963cad8f2733021e87a026
Jaime Lopez <detec.azul@gmail.com>
2018-06-13 12:41:18 +0100

    CSS for centering and keeping aspect ratio. Field can render X or O depending on who played

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -7,68 +7,77 @@ const Grid = (props) => {
     <div className="grid">
       <div className="row">
         <Field
+          currentPlayer={props.currentPlayer}
           fieldId="1"
           x="1"
           y="1"
-          clickedFields={props.clickedFields}
+          // clickedFields={props.clickedFields}
           onClick={props.handleFieldClick}/>
 
         <Field
+          currentPlayer={props.currentPlayer}
           fieldId="2"
           x="2"
           y="1"
-          clickedFields={props.clickedFields}
+          // clickedFields={props.clickedFields}
           onClick={props.handleFieldClick}/>
 
         <Field
+          currentPlayer={props.currentPlayer}
           fieldId="3"
           x="3"
-          y="3"
-          clickedFields={props.clickedFields}
+          y="1"
+          // clickedFields={props.clickedFields}
           onClick={props.handleFieldClick}/>
       </div>
       <div className="row">
         <Field
+          currentPlayer={props.currentPlayer}
           fieldId="4"
           x="1"
           y="2"
-          clickedFields={props.clickedFields}
+          // clickedFields={props.clickedFields}
           onClick={props.handleFieldClick}/>
 
         <Field
+          currentPlayer={props.currentPlayer}
           fieldId="5"
           x="2"
           y="2"
-          clickedFields={props.clickedFields}
+          // clickedFields={props.clickedFields}
           onClick={props.handleFieldClick}/>
 
         <Field
+          currentPlayer={props.currentPlayer}
           fieldId="6"
           x="3"
           y="2"
-          clickedFields={props.clickedFields}
+          // clickedFields={props.clickedFields}
           onClick={props.handleFieldClick}/>
       </div>
       <div className="row">
         <Field
+          currentPlayer={props.currentPlayer}
           fieldId="7"
           x="1"
           y="3"
-          clickedFields={props.clickedFields}
+          // clickedFields={props.clickedFields}
           onClick={props.handleFieldClick}/>
 
         <Field
+          currentPlayer={props.currentPlayer}
           fieldId="8"
           x="2"
           y="3"
-          clickedFields={props.clickedFields}
+          // clickedFields={props.clickedFields}
           onClick={props.handleFieldClick}/>
-          
+
         <Field
+          currentPlayer={props.currentPlayer}
           fieldId="9"
           x="3"
           y="3"
-          clickedFields={props.clickedFields}
+          // clickedFields={props.clickedFields}
           onClick={props.handleFieldClick}/>
       </div>
     </div>
